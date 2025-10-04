1b6a84901dccbcb62a642218e2ad8029b164c417
Jessica Apuyan <jessicaapuyan2@gmail.com>
2025-10-04 15:29:05 +0800

    Laboratory experiment# 1 by Japuyan

---
+++ convolution/Japuyan/scipy_convolution.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import convolve
+
+# Define the input signals
+x = np.array([71, 72, 73, 74])  # Example input signal
+h = np.array([5, 7, 5])  # Example impulse response
+
+# Perform convolution using scipy
+y = convolve(x, h, mode='full')
+
+# Plot the results
+plt.figure(figsize=(10, 4))
+plt.stem(y, basefmt=" ")
+plt.title("Result of Convolution using scipy.signal.convolve")
+plt.xlabel("Sample index")
+plt.ylabel("Amplitude")
+plt.grid()
+plt.show()
